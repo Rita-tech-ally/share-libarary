c3666633f2dbc8b3df56dd0ba844abb85098da56
¨Rita-tech-ally¨ <ritudogra586@gmail.in>
2026-05-17 22:08:52 +0530

    Added shared library functions

--- vars/printGreeting.groovy
+++ vars/printGreeting.groovy
@@ -1,6 +1,4 @@
-def call(String name, String todayDate) {
-    // Extract day if full date is provided (e.g., 17-05-2026 or 2026-05-17)
-    def day = todayDate.tokenize('-')[-1]
-
-    echo " Hi '${name}', today is '${day}'"
+def call(String name, String date) {
+    def day = date.split("-")[2]
+    echo "Hi ${name}, today is ${day}"
 }
